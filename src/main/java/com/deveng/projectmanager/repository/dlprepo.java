0dd6df7084dcd95f98f0b0a2333489f318dcf179
Karthik Renuprasad <krenuprasad@Karthiks-MacBook-Pro.local>
2023-07-25 12:02:17 -0700

    initial project checkin

---
+++ src/main/java/com/deveng/projectmanager/repository/dlprepo.java
@@ -0,0 +1,8 @@
+package com.deveng.projectmanager.repository;
+
+import com.deveng.projectmanager.model.dlp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface dlprepo extends JpaRepository<dlp, Long>{
+    
+}
